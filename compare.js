bcc9ef249b3131b91b5812b8a7f30a615a087836
juan david calderon jimenez <j.calderonj@uniandes.edu.co>
2023-05-11 23:40:46 -0500

    Compare iteration done

--- compare.js
+++ compare.js
@@ -1,33 +1,124 @@
+const compareImages = require("resemblejs/compareImages")
 const fs = require('fs');
-const resemble = require('resemblejs');
+class Compare {
+  resultInfo = []
+  images_3_41_1 = []
+  images_4_44 = []
+  constructor(fromRootFolder, escenaryName, browsers, options, V3_41_1Folder, V4_44Folder) {
+    this.fromRootFolder = fromRootFolder; // screenshotsRoot
+    this.escenaryName = escenaryName; // folderName
+    this.browsers = browsers;
+    this.options = options;
+    this.V3_41_1Folder = V3_41_1Folder;
+    this.V4_44Folder = V4_44Folder;
+  }
 
-const image1Path = 'imagenes/eliminar_post_p3_before.png'; // Ruta a la primera imagen
-const image2Path = 'imagenes/eliminar_post_p3.png'; // Ruta a la segunda imagen
+  checkBrowsers() {
+    return this.browsers.length > 0;
+  }
 
-// Lee las imágenes
-const image1 = fs.readFileSync(image1Path);
-const image2 = fs.readFileSync(image2Path);
+  async compareImages() {
+    for (let b of this.browsers) {
+      if (!['chromium', 'webkit', 'firefox'].includes(b)) {
+        return false;
+      }
+      if (!fs.existsSync(`./results/${this.escenaryName}`)) {
+        fs.mkdirSync(`./results/${this.escenaryName}`, { recursive: true });
+      }
 
-// Configuración de Resemble.js
-const resembleConfig = {
-  output: {
-    errorColor: {
-      red: 255,
-      green: 0,
-      blue: 255
-    },
-    errorType: 'movement',
-    transparency: 0.3,
-    largeImageThreshold: 1200
-  },
-  scaleToSameSize: true,
-  ignore: 'antialiasing'
-};
+      fs.readdirSync(`${this.fromRootFolder}/${this.escenaryName}/${this.V3_41_1Folder}`).forEach(file => {
+        const newName = file.replace('.png', '');
+        this.images_3_41_1.push({
+          nombre: `before-${newName}`,
+          value: fs.readFileSync(`${this.fromRootFolder}/${this.escenaryName}/${this.V3_41_1Folder}/${file}`),
+          path: `${this.fromRootFolder}/${this.escenaryName}/${this.V3_41_1Folder}/${file}`
+        });
+        this.images_4_44.push({
+          nombre: `after-${newName}`,
+          value: fs.readFileSync(`${this.fromRootFolder}/${this.escenaryName}/${this.V4_44Folder}/${file}`),
+          path: `${this.fromRootFolder}/${this.escenaryName}/${this.V4_44Folder}/${file}`
+        })
+      });
 
-// Compara las imágenes utilizando Resemble.js
-resemble(image1)
-  .compareTo(image2)
-  .ignoreColors()
-  .onComplete(function(data) {
-    console.log(data); // Imprime los resultados de la comparación
-  });
+      let data;
+      for (let i = 0; i < this.images_3_41_1.length; i++) {
+        data = await compareImages(
+          this.images_3_41_1[i].value,
+          this.images_4_44[i].value,
+          this.options
+        );
+        this.resultInfo.push({
+          [b]: {
+            compareFile: `${this.images_3_41_1[i].nombre}-${this.images_4_44[i].nombre}`,
+            compareFilePath: `./compare-${b}-${this.images_3_41_1[i].nombre}-${this.images_4_44[i].nombre}.png`,
+            isSameDimensions: data.isSameDimensions,
+            dimensionDifference: data.dimensionDifference,
+            rawMisMatchPercentage: data.rawMisMatchPercentage,
+            misMatchPercentage: data.misMatchPercentage,
+            diffBounds: data.diffBounds,
+            analysisTime: data.analysisTime
+          }
+        });
+        fs.writeFileSync(`./results/${this.escenaryName}/compare-${b}-${this.images_3_41_1[i].nombre}-${this.images_4_44[i].nombre}.png`, data.getBuffer());
+      }
+      fs.writeFileSync(`./results/${this.escenaryName}/report.html`, this.createReport(this.escenaryName, this.resultInfo, this.images_3_41_1, this.images_4_44));
+    }
+    fs.copyFileSync('./index.css', `./results/${this.escenaryName}/index.css`);
+    console.log('------------------------------------------------------------------------------------')
+    console.log("Execution finished. Check the report under the results folder")
+    return this.resultInfo;
+  }
+
+  createReport(folderName, resultInfo, images_3_41_1, images_4_44) {
+    return `
+      <html>
+          <head>
+              <title> VRT Report for ${folderName} </title>
+              <link href="index.css" type="text/css" rel="stylesheet">
+          </head>
+          <body>
+              <h1>Report for 
+                ${folderName}
+              </h1>
+              <div id="visualizer">
+                  ${config.browsers.map(b => browser(b, resultInfo, images_3_41_1, images_4_44))}
+              </div>
+          </body>
+      </html>`
+  }
+
+  browser(b, resultInfo, images_3_41_1, images_4_44) {
+    return `<div class=" browser" id="test0">
+      <div class=" btitle">
+          <h2>Browser: ${b}</h2>
+      </div>
+      ${resultInfo.map((dataInfo, index) => {
+      return `
+                <div class=" btitle">
+                  <p>Data: ${JSON.stringify(dataInfo)}</p>
+                </div>
+                <div class="imgline">
+                  <div class="imgcontainer">
+                    <span class="imgname">Reference</span>
+                    <img class="img2" src="../../${images_3_41_1[index].path}" id="refImage" label="Reference">
+                  </div>
+                  <div class="imgcontainer">
+                    <span class="imgname">Test</span>
+                    <img class="img2" src="../../${images_4_44[index].path}" id="testImage" label="Test">
+                  </div>
+                </div>
+                <div class="imgline">
+                  <div class="imgcontainer">
+                    <span class="imgname">Diff</span>
+                    <img class="imgfull" src="${dataInfo[b].compareFilePath}" id="diffImage" label="Diff">
+                  </div>
+                </div>`
+
+    })
+      }
+    </div>`
+  }
+}
+
+
+module.exports = new Compare();
